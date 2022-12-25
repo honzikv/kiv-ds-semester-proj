f26a8994a9b1d86cf3fc47460e1f4728ce9b2ac7
vhonzik <vaclavhonzik98@gmail.com>
2022-12-25 13:14:22 +0100

    store api

--- ex03/node/store.py
+++ ex03/node/store.py
@@ -1,3 +1,5 @@
+import cluster_connector
+
 from fastapi import APIRouter
 
 # Simple module that represents a data store
@@ -8,11 +10,44 @@ _store = {}
 store_router = APIRouter()
 
 
-@store_router.get('/')
-async def get_item(item: str):
-    if item in _store:
+@store_router.get('/{key}')
+async def get_item(key: str):
+    if key in _store:
         # TODO perform master check or something
-        return {'success': True, 'item': _store[item]}
-    
-    # Otherwise try to contact bottom or top node
-    
+        return {'item': _store[key]}
+
+    # Otherwise try to contact the top node
+    res = await cluster_connector.get_key_from_parent(key)
+
+    # Update the key in the store
+    item = res['item']
+    _store[key] = item
+
+    # Return the value
+    return {'success': item is not None, 'item': item}
+
+
+@store_router.put('/{key}')
+async def put_item(key: str, value):
+    # Update the value locally
+    _store[key] = value
+
+    # Update it in the parent node
+    res = await cluster_connector.put_key_in_parent(key, value)
+    if not res['success']:
+        return {'success': False, 'error': 'Failed to update parent node', 'key': key, 'value': value}
+
+    return {'success': True, 'key': key, 'value': value}
+
+
+@store_router.delete('/{key}')
+async def delete_item(key: str):
+    # Delete the value locally
+    _store.pop(key, None)
+
+    # Delete it in the parent node
+    res = await cluster_connector.delete_key_from_parent(key)
+    if not res['success']:
+        return {'success': False, 'error': 'Failed to delete from parent node', 'key': key}
+
+    return {'success': True, 'key': key}
